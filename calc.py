682c63ce7ee7000957b57e88735216538b2377c7
aronsx <aronsx@ya.ru>
2020-07-15 02:08:43 +0300

    remove ru comment

--- calc.py
+++ calc.py
@@ -1,4 +1,6 @@
-user_string = input('Введите вырежение: ').split()
+user_string = input('enter expression').split()
+
+
 # user_string = ['2', '+', '2', '2', '-', '1', '*', '2', '/', '3']
 # user_string = ['2', '+', '2', '-', '1', '*', '2', '/', '3']
 # user_string = ['2', '+', '2', '*', '2', '*']
@@ -11,20 +13,20 @@ class MathError(Exception):
 
 try:
     while len(user_string) != 1:
-        # первый продод - умножение и деление
-        if "*" in user_string or "/" in user_string:  # чтобы не проходить второй раз цикл
+        # first pass: division multiplication
+        if "*" in user_string or "/" in user_string:  # not to pass a second time
             for i, elem in enumerate(user_string):
                 if elem.isnumeric() and user_string[i + 1].isnumeric():
                     raise MathError
                 if not elem.isnumeric():
                     if elem == '*':
                         r, l = user_string.pop(i + 1), user_string.pop(i - 1)
-                        # смещаем индекс влево, так как мы удалили перед ним элемент
+                        # shift the index to the lefT
                         user_string[i - 1] = str(float(l) * float(r))
                     elif elem == '/':
                         r, l = user_string.pop(i + 1), user_string.pop(i - 1)
                         user_string[i - 1] = str(float(l) / float(r))
-        # второй проход - сложнеие и вычитание
+        # second pass: addition and subtraction
         for i, elem in enumerate(user_string):
             if elem == '+':
                 r, l = user_string.pop(i + 1), user_string.pop(i - 1)
@@ -35,10 +37,10 @@ try:
 
 
 except MathError as me:
-    print('Не верное выражение')
+    print('wrong expression')
 except ValueError as ve:
-    print(f'Не верное выражение: два оператора подряд')
+    print(f'wrong expression: two operand')
 except IndexError as ie:
-    print(f'Не верное выражение: оператор без операнда')
+    print(f'wrong expression: operator without operand')
 
-print(f'Результат: {user_string[0] if len(user_string) == 1 else "ошибка вычислений"}')
+print(f'result: {user_string[0] if len(user_string) == 1 else "error calculation"}')
